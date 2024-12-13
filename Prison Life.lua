e742ec9df3bfe391be27db3572b1e5637ff25ca7
Matjias <117152858+denrigtigeMatjias@users.noreply.github.com>
2024-12-13 16:53:44 +0100

    Update Prison Life.lua

--- Prison Life.lua
+++ Prison Life.lua
@@ -8,11 +8,9 @@ local gun = game.Workspace.Prison_ITEMS.giver:GetChildren()
 local inf = 999999999999999999999999999999999999999999999999999999999999999999999
 
 Guns:Button("Infinite Ammo",function()
-    MaxAmmoM9 = require(game.Players.LocalPlayer.Backpack.M9.GunStates)
-    MaxAmmoM9.MaxAmmo = inf
-    CurrentAmmoM9 = require(game.Players.LocalPlayer.Backpack.M9.GunStates)
-    CurrentAmmoM9.CurrentAmmo = inf
-    Spread = require(game.Players.LocalPlayer.Backpack.M9.GunStates)
-    Spread.Spread.Min = 0
-    Spread.Spread.Max = 0
+    M9GunStates = require(game.Players.LocalPlayer.Backpack.M9.GunStates)
+    M9GunStates.MaxAmmo = inf
+    M9GunStates.CurrentAmmo = inf
+    M9GunStates.Spread.Min = 0
+    M9GunStates.Spread.Max = 0
 end)
